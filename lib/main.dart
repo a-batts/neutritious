be8c27c57f62c49db14ae02331c530f70b421ab7
Alex Batts <alex.batts05@gmail.com>
2024-03-23 17:40:08 -0400

    Category page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neutritious/category_page.dart';
 
 import 'enums/menu_category.dart';
 
@@ -33,24 +34,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MenuPage(title: 'Flutter Demo Home!'),
+      home: const MenuPage(),
     );
   }
 }
 
 class MenuPage extends StatefulWidget {
-  const MenuPage({super.key, required this.title});
+  const MenuPage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   State<MenuPage> createState() => _MenuPageState();
@@ -68,7 +62,13 @@ class _MenuPageState extends State<MenuPage> {
           color: category.color,
           child: InkWell(
             borderRadius: BorderRadius.circular(12.0),
-            onTap: () => {},
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => CategoryPage(category: category)),
+              )
+            },
             child: Padding(
                 padding: const EdgeInsets.symmetric(
                     horizontal: 22.0, vertical: 20.0),
