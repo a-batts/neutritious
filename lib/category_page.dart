c04bc96aa21e40ce691339ec6cf9aa7a161aa15d
Alex Batts <alex.batts05@gmail.com>
2024-03-24 08:54:07 -0400

    Fix missing import

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -5,6 +5,7 @@ import 'package:neutritious/db/database_service.dart';
 import 'package:neutritious/db/user_menu_item.dart';
 import 'package:neutritious/enums/menu_category.dart';
 import 'package:neutritious/new_form_dialog.dart';
+import 'package:neutritious/random_page.dart';
 
 import 'item_page.dart';
 
@@ -26,16 +27,16 @@ class _CategoryPageState extends State<CategoryPage> {
     return (SizedBox(
       width: 600,
       child: Card(
-        clipBehavior: Clip.antiAlias,
+          clipBehavior: Clip.antiAlias,
           child: InkWell(
-        borderRadius: BorderRadius.circular(12.0),
-        onTap: () => {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ItemPage(item: data)),
-          )
-        },
-        child: Row(
+            borderRadius: BorderRadius.circular(12.0),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ItemPage(item: data)),
+              )
+            },
+            child: Row(
               children: [
                 Row(
                   children: [
@@ -49,20 +50,22 @@ class _CategoryPageState extends State<CategoryPage> {
                       ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Row(children: [ const SizedBox(
-                        width: 12.0,
-                      ),
-                      Text(
-                        data.title,
-                        style: const TextStyle(
-                            fontSize: 18.0, fontWeight: FontWeight.w500),
-                      )]),
+                      child: Row(children: [
+                        const SizedBox(
+                          width: 12.0,
+                        ),
+                        Text(
+                          data.title,
+                          style: const TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.w500),
+                        )
+                      ]),
                     ),
                   ],
                 )
               ],
             ),
-      )),
+          )),
     ));
   }
 
@@ -77,8 +80,8 @@ class _CategoryPageState extends State<CategoryPage> {
                 category: widget.category,
               ),
             )).then((x) {
-              setState(() {});
-               });
+      setState(() {});
+    });
   }
 
   @override
@@ -106,63 +109,63 @@ class _CategoryPageState extends State<CategoryPage> {
           child: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(category.icon, size: 40.0),
-              const SizedBox(
-                width: 12.0,
-              ),
-              Text(category.title,
-                  style: const TextStyle(
-                      fontSize: 30.0, fontWeight: FontWeight.w600))
-            ]),
-            Wrap(runSpacing: 16.0, children: <Widget>[
-              FutureBuilder(
-                  future: items,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      if (snapshot.data?.isEmpty == true) {
-                        return const SizedBox(
-                            width: 600, child: Card(child: Text("No data")));
-                      } else {
-                        var items = snapshot.data;
-                        return Column(children: [
-                          SizedBox(
-                            width: 600.0,
-                            child: FilledButton.icon(
-                                onPressed: () => selectRandomItem(items!),
-                                label: const Padding(
-                                    padding: EdgeInsets.all(12.0),
-                                    child: Text("RANDOM",
-                                        style: TextStyle(fontSize: 22.0))),
-                                icon: const Icon(Icons.casino),
-                                style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(6.0)))),
-                          ),
-                          const SizedBox(height: 16.0),
-                          ...items?.map((item) => _itemCard(item)) ?? []
-                        ]);
-                      }
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(category.icon, size: 40.0),
+            const SizedBox(
+              width: 12.0,
+            ),
+            Text(category.title,
+                style: const TextStyle(
+                    fontSize: 30.0, fontWeight: FontWeight.w600))
+          ]),
+          Wrap(runSpacing: 16.0, children: <Widget>[
+            FutureBuilder(
+                future: items,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data?.isEmpty == true) {
+                      return const SizedBox(
+                          width: 600, child: Card(child: Text("No data")));
                     } else {
-                      return const CircularProgressIndicator();
+                      var items = snapshot.data;
+                      return Column(children: [
+                        SizedBox(
+                          width: 600.0,
+                          child: FilledButton.icon(
+                              onPressed: () => selectRandomItem(items!),
+                              label: const Padding(
+                                  padding: EdgeInsets.all(12.0),
+                                  child: Text("RANDOM",
+                                      style: TextStyle(fontSize: 22.0))),
+                              icon: const Icon(Icons.casino),
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(6.0)))),
+                        ),
+                        const SizedBox(height: 16.0),
+                        ...items?.map((item) => _itemCard(item)) ?? []
+                      ]);
                     }
-                  }),
-              SizedBox(
-                width: 600.0,
-                child: FilledButton.icon(
-                    onPressed: () => openDialog(),
-                    label: const Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Text("WRITE YOUR OWN",
-                            style: TextStyle(fontSize: 22.0))),
-                    icon: const Icon(Icons.add),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6.0)))),
-              ),
-            ])
-          ],
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }),
+            SizedBox(
+              width: 600.0,
+              child: FilledButton.icon(
+                  onPressed: () => openDialog(),
+                  label: const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text("WRITE YOUR OWN",
+                          style: TextStyle(fontSize: 22.0))),
+                  icon: const Icon(Icons.add),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6.0)))),
+            ),
+          ])
+        ],
       )),
     );
   }
